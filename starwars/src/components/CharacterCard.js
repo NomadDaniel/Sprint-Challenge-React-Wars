21320e57c3c5d373ebd41445f16baa9ef4425368
NomadDaniel <reachingdaniel@gmail.com>
2020-04-19 18:29:16 -0400

    this branch is for refactoring sprint w rick and morty api

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,17 +1,18 @@
 import React from "react"
 import styled from "styled-components"
 
+
 const Card = styled.div`
-    width: 35%;
-    margin: 5%;
+    width: 25%;
+    margin: 1%;
     background: white;
-    border-radius: 5%;
+    border-radius: 15%;
     // margin: 3%;
-    padding: 1%;
-    opacity: 70%;
+    // padding: 1%;
+    // opacity: 90%;
 `
 
-const CardTitle = styled.h3`
+const CardTitle = styled.h1`
     // margin-top: 0.1rem;
     // padding: 1.3rem 0;
     font-size: 1.7rem;
@@ -21,20 +22,29 @@ const CardTitle = styled.h3`
 const CardP = styled.p`
     color: darkgreen;
     font-size: 1rem;
-    
     text-align: center;
 `
 
-const CharacterCard = props =>{
+const CharacterCard = props => {
+  const {name, status, species, gender, origin, location, image} = props.item
   return (
-    <Card key={props.key}>
+    <Card>
+      {/* <img src={props.image} alt={props.name}/>
       <CardTitle>{props.name}</CardTitle>
+      <CardP>Status: {props.status}</CardP>
+      <CardP>Species: {props.species}</CardP>
       <CardP>Gender: {props.gender}</CardP>
-      <CardP>Height: {props.height}</CardP>
-      <CardP>Mass: {props.mass}</CardP>
-      <CardP>Skin Color: {props.skinColor}</CardP>
+      <CardP>Origin: {props.origin}</CardP>
+      <CardP>Location: {props.location}</CardP> */}
+      <img src={image} alt={name}/>
+      <CardTitle>{name}</CardTitle>
+      <CardP>Status: {status}</CardP>
+      <CardP>Species: {species}</CardP>
+      <CardP>Gender: {gender}</CardP>
+      <CardP>Origin: {origin.name}</CardP>
+      <CardP>Location: {location.name}</CardP>
     </Card>
-    )
+  )
 }
 
 export default CharacterCard
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -8,30 +8,75 @@ const ListContainer = styled.div`
     width: 90%;
     margin: 0 auto;
     display: flex;
+    justify-content: space-between;
     flex-flow: row wrap;
+    // flex-direction: coloumn;
 `
+const ButtonDiv = styled.div`
+disply:flex;
+justify-content: space-between;
+width: 100%;
+height: 100%;
+flex-flow: row wrap;
+`
+const Button = styled.button`
+    height: 15%;
+    width: 15%;
+    margin: 1%;
+    background: yellow;
+    border-radius: 1%;
+    padding: 1%;
+    color: blue;
+    font-weight: bold;
+    font-size: 1.2rem;
+
+  `
 
 function CharacterList() {
     const [people, setPeople] = useState([])
+    const [page, setPage] = useState(1)
 
     useEffect(() => {
         axios
-            .get(`https://swapi.py4e.com/api/people/`)
+            .get(`https://rickandmortyapi.com/api/character/?page=${page}`)
             .then(res=>{
                 console.log(res);
                 setPeople(res.data.results);
+                // console.log(page)
             })
-    }, [])
+    }, [page])
+
+    const handlePrevious = e =>{
+      setPage(page-1)
+    }
+
+    const handleNext = e =>{
+      setPage(page+1)
+    }
 
     return (
       <ListContainer>
+        <ButtonDiv> 
+        <Button onClick={handlePrevious}>Prev</Button> 
+        <Button onClick={handleNext}>Next</Button>
+        </ButtonDiv>
         {people.map(item => {
           return (
-            <CharacterCard key={ item.index } name={ item.name } gender={ item.gender } height={ item.height } mass={ item.mass } skinColor={ item.skin_color }
-            />
+              <CharacterCard 
+              key={ item.id } 
+              // name={ item.name } 
+              // status={ item.status }
+              // species={ item.species }
+              // gender={ item.gender }
+              // origin={ item.origin.name } 
+              // location={ item.location.name }
+              // image = {item.image}
+              item ={item}
+              />
             )
           })
         }
+        
       </ListContainer>
     )
 }
